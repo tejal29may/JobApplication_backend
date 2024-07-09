23173034c579350beb4d32dcf53e7952238e76f9
tejal29may <85352094+tejal29may@users.noreply.github.com>
2024-07-09 20:39:59 +0530

    env

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const express = require("express");
 const mongoose = require("mongoose")
 const app = express()
 app.use(express.json())
+const dotenv=require("dotenv")
 
 const jobRouter = require("./Routes/jobs")
 
 // mongodb connection
-mongoose.connect("mongodb+srv://tejalmohod5:ZcPAFamk8CI5ZTyr@jobapplicationbackend.yobblxb.mongodb.net/ ")
+dotenv.config();
+console.log(process.env.DATABASE_URI);
+mongoose.connect(process.env.DATABASE_URI)
     .then(() => {
         console.log("connection successfull");
     })
